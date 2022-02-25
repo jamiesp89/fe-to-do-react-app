5a9be9e50ecba8541b5f4dbb5fe77a99ac92e94a
jamiesp89 <jscot89@hotmail.com>
2022-02-25 11:03:38 +0000

    Setup react to-do app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [list, setList] = useState([
+    "trevor",
+    "report mezz to rozzers",
+    "task 3",
+  ]);
+
+  const clearList = () => {
+    setList([]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mezz and Jamie's To-do app</h1>
       </header>
+      <p>To-do</p>
+      <ul>
+        <li>{list[0]}</li>
+        <li>{list[1]}</li>
+        <li>{list[2]}</li>
+      </ul>
+      <button onClick={() => clearList()}>clear all</button>
     </div>
   );
 }
